bcf4d3417e8a9540347c6fd7a08bdbfdaa9d11dc
annaloggg <atp49@pitt.edu>
2024-11-05 19:37:32 -0500

    fixed?

--- lyric_lingo/lib/Models/song_model.dart
+++ lyric_lingo/lib/Models/song_model.dart
@@ -2,10 +2,10 @@ class SongModel {
   String id;
   String name;
   String language;
-  String[] artists;
-  Float duration;
-  Bool explicit;
-  String[] lyrics;
+  List<String> artists;
+  double duration;  // Using double instead of Float for Dart
+  bool explicit;
+  List<String> lyrics;
 
   SongModel({
     required this.id,
