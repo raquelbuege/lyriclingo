5228c695855b7e01bdd978c21344edf493cbff9d
Raquel Buege <rlb143@pitt.edu>
2024-11-18 16:40:02 -0500

    api integration

--- lyric_lingo/android/app/src/main/kotlin/com/example/lyric_lingo/MainActivity.kt
+++ lyric_lingo/android/app/src/main/kotlin/com/example/lyric_lingo/MainActivity.kt
@@ -1,5 +1,56 @@
 package com.example.lyric_lingo
-
+import android.net.Uri
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.Log
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.lyric_lingo"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        authenticateConnectionSpotify()
+    }
+    
+    private fun authenticateConnectionSpotify(): Boolean {
+
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "authenticate") {
+                val clientId = call.argument<String>("clientId")
+                val clientSecret = call.argument<String>("clientSecret")
+                val redirectUri = "lyriclingoapp://main";
+                if (clientId != null && clientSecret != null) {
+                    val buildRequest = AuthorizationRequest.Builder(
+                        clientId,
+                        AuthorizationResponse.Type.TOKEN,
+                        redirectUri,
+                    )
+                    buildRequest.setScopes(arrayOf("streaming"))
+                    val logInUser = buildRequest.build()
+                    AuthorizationClient.openLoginInBrowser(this, logInUser)
+                    result.success("Success!")
+                } else {
+                    result.error("Error!", "Not working!", null)
+                }
+            }
+        }
+        return true;
+    }
+    private fun logInUser(clientId: String,redirectUri: String): Boolean {
+        val builder = AuthorizationRequest.Builder(
+            clientId,
+            AuthorizationResponse.Type.TOKEN,
+            redirectUri,
+        )
+        builder.setScopes(arrayOf("streaming"))
+        val request = builder.build()
+        AuthorizationClient.openLoginInBrowser(this, request)
+        return true;
+    }
+}      
+                
+
