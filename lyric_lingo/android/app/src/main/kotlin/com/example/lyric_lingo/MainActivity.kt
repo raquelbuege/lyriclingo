724888d167dbf8a83b2fe4b8a5d329fabee4551a
Anna Plazek <atp49@pitt.edu>
2024-11-18 21:47:19 -0500

    Squashed commit of the following:
    commit 39b3c998909310d8b8bfee5ac07f6d0d0c435b91
    Merge: e268a09 6d1e395
    Author: dorothy <dorothycrw002@gmail.com>
    Date:   Mon Nov 18 21:26:04 2024 -0500
    
        updated api and back end stuff
    
    commit 6d1e395449c204023c8a00dabce50d99ed532c98
    Merge: a25cfb0 5228c69
    Author: raquel buege <121844475+raquelbuege@users.noreply.github.com>
    Date:   Mon Nov 18 16:40:39 2024 -0500
    
        Merge pull request #9 from raquelbuege/raquel_branch
    
        api integration
    
    commit 5228c695855b7e01bdd978c21344edf493cbff9d
    Author: Raquel Buege <rlb143@pitt.edu>
    Date:   Mon Nov 18 16:40:02 2024 -0500
    
        api integration
    
    commit e268a094ba14b5eacfafadc6c8d33d59fb5c153c
    Author: dorothy <dorothycrw002@gmail.com>
    Date:   Mon Nov 18 16:36:32 2024 -0500
    
        branch of non adjusted android files

--- lyric_lingo/android/app/src/main/kotlin/com/example/lyric_lingo/MainActivity.kt
+++ lyric_lingo/android/app/src/main/kotlin/com/example/lyric_lingo/MainActivity.kt
@@ -1,5 +1,82 @@
 package com.example.lyric_lingo
-
+import android.net.Uri
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.Log
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
+import android.content.Intent
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.lyric_lingo"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        authenticateConnectionSpotify()
+    }
+    
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val response = AuthorizationClient.getResponse(resultCode, data)
+
+        if (response.type == AuthorizationResponse.Type.TOKEN) {
+            val accessToken = response.accessToken
+
+            // Save the access token securely
+            val sharedPreferences = getSharedPreferences("spotify_prefs", MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString("access_token", accessToken)
+            editor.apply()
+
+            Log.d("SpotifyAuth", "Access token saved successfully!")
+        } else if (response.type == AuthorizationResponse.Type.ERROR) {
+            Log.e("SpotifyAuth", "Authorization error: ${response.error}")
+        }
+    }
+
+    private fun authenticateConnectionSpotify(): Boolean {
+
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "authenticate" -> {
+                    val clientId = call.argument<String>("clientId")
+                    val clientSecret = call.argument<String>("clientSecret")
+                    val redirectUri = "lyriclingoapp://main";
+
+                    if (clientId != null && clientSecret != null) {
+                        val buildRequest = AuthorizationRequest.Builder(
+                            clientId,
+                            AuthorizationResponse.Type.TOKEN,
+                            redirectUri,
+                        )
+                        buildRequest.setScopes(arrayOf("streaming"))
+                        val logInUser = buildRequest.build()
+                    
+                        AuthorizationClient.openLoginInBrowser(this, logInUser)
+                        result.success("Success!")
+                    } else {
+                        result.error("Error!", "Not working!", null)}
+                }
+
+                "getAccessToken" -> {
+                    val sharedPreferences = getSharedPreferences("spotify_prefs", MODE_PRIVATE)
+                    val accessToken = sharedPreferences.getString("access_token", null)
+                    if (accessToken != null) {
+                        result.success(accessToken)
+                    } else {
+                        result.error("Error", "Access token not found!", null)
+                    }
+                }
+
+            }
+    
+            }
+            return true;
+        }
+    }
+     
+                
+
